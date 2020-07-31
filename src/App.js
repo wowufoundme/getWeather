a8e8498270fc355a876405e1652162d76b2412e3
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-07-31 22:07:16 +0530

    theme and api imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Input from './Input/Input.component';
+import fetchData from './api/fetchData';
 
 import './App.css';
 
 const App = () => {
+
+    const [ city, setCity ] = useState('');
+
+    const getData = async () => {
+        const data = await fetchData(city);
+        console.log(data);
+    }
+
     return (
-        <div>
-            <h1>Weather App</h1>
+        <div className='App background--slideshow'>
+            <Input
+                city={city}
+                setCity={setCity}
+                getData={getData}
+            />
         </div>
     );
 }
