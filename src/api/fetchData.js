ffe0aec61fa9a36321ec8344d27888ab8aca0c95
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-08-01 01:53:31 +0530

    change units for measurement

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_KEY = "3f63b541464c9d678aa6041f9abf4488";
 
 const fetchData = async (city) => {
-    const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
+    const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`);
 
     return response.data;
 }
