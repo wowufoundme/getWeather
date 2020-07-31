40f94091c80718253e9b40aad78062ea8012371f
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-07-31 22:10:30 +0530

    add public api key

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_KEY = "3f63b541464c9d678aa6041f9abf4488";
+
 const fetchData = async (city) => {
-    const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=process.env.API_KEY`);
+    const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
 
     return response.data;
 }
