13bbcce2676b12fba99f3ae87b5badbb28ce6007
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-08-02 01:15:36 +0530

    Updates

---
+++ precache-manifest.5fc0a0f3cb2f775cbadeac5778156508.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c62d219ba3030bdf64d6ac78b206874",
+    "url": "/getWeather/index.html"
+  },
+  {
+    "revision": "ad523506fe4c544274fe",
+    "url": "/getWeather/static/css/main.02c59741.chunk.css"
+  },
+  {
+    "revision": "8e862008210139513015",
+    "url": "/getWeather/static/js/2.1dc7e581.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/getWeather/static/js/2.1dc7e581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad523506fe4c544274fe",
+    "url": "/getWeather/static/js/main.9ff095c9.chunk.js"
+  },
+  {
+    "revision": "0bbb877445a2085f30c1",
+    "url": "/getWeather/static/js/runtime-main.0b69a591.js"
+  },
+  {
+    "revision": "e8b7da6e2b73d85f51bd01f1d18180b8",
+    "url": "/getWeather/static/media/wallpaper.e8b7da6e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wowufoundme.js.org/getWeather/precache-manifest.d0f7ff17f29935a1acfd4aff1c9b8a2d.js"
+  "/getWeather/precache-manifest.5fc0a0f3cb2f775cbadeac5778156508.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wowufoundme.js.org/getWeather/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/getWeather/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
