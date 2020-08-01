dc407491c71053756763ada5f963e4647039d187
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-08-02 01:10:40 +0530

    Updates

---
+++ precache-manifest.a24119352b86c92467046126647abe96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f80898cc011be1f87b344b0b8e4eae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e55b510a864ae8843cf2",
+    "url": "/static/css/main.b2a33144.chunk.css"
+  },
+  {
+    "revision": "8e862008210139513015",
+    "url": "/static/js/2.1dc7e581.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1dc7e581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e55b510a864ae8843cf2",
+    "url": "/static/js/main.9ff095c9.chunk.js"
+  },
+  {
+    "revision": "2b5cc2ceb15d2de35643",
+    "url": "/static/js/runtime-main.eef8dfd8.js"
+  },
+  {
+    "revision": "e8b7da6e2b73d85f51bd01f1d18180b8",
+    "url": "/static/media/wallpaper.e8b7da6e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/getweather/precache-manifest.16125b65185940f950e3072665a0cc1c.js"
+  "/precache-manifest.a24119352b86c92467046126647abe96.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/getweather/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
