b47e9599d38b81d078297e1eff63164270056804
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2022-02-25 19:17:25 -0500

    update theme and many changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import Input from './Input/Input.component';
 import WeatherCard from './WeatherCard/WeatherCard.component';
-import SearchResults from './SearchResults/SearchResults.component';
+// import SearchResults from './SearchResults/SearchResults.component';
 import fetchData from './api/fetchData';
 import searchData from './api/searchData';
 import Header from './Header/Header.component';
@@ -14,7 +14,8 @@ const App = () => {
     const [ city, setCity ] = useState('');
     const [ cityData, setCityData ] = useState({});
     const [ displayData, setDisplayData ] = useState(false);
-    const [ theme, setTheme ] = useState('');
+    const [ theme, setTheme ] = useState('dark');
+    const [ backdrop, setBackdrop ] = useState('./assets/mountains.jpg');
     let [ searchResults, setSearchResults ] = useState([]);
     
     const getData = async (city='delhi') => {
@@ -28,6 +29,7 @@ const App = () => {
     const changeTheme = e => {
         if (theme === '') {
             setTheme('dark');
+
         } else {
             setTheme('');
         }
@@ -51,29 +53,42 @@ const App = () => {
         setCity('');
         setDisplayData(false);
     }
+
  
     return (
         <div className='App background--slideshow'>
-            <Header title='Get Weather' />
-            <Input
-                city={city}
-                setCity={setCity}
-                getData={getData}
-            />
-            { displayData && <WeatherCard data={cityData} /> }
-            {
-                city === '' ?
-                null :
-                <SearchResults setCity={setCity} getData={getData} data={searchResults} />
-            }
-            <button className='themer posLeft' onClick={changeTheme}>
-            {
-                theme === '' ?
-                "Dark Theme" :
-                "Light Theme"
-            }
-            </button>
-            <button className='themer posRight btn--invert' onClick={() => clearData()}>Clear</button>
+            <div className='backdrop--display__half'></div>
+            <div className='container'>
+
+                <div className='left--section'>
+                    {/* <Header title='get weatherrrr' /> */}
+                    <Input
+                        city={city}
+                        setCity={setCity}
+                        getData={getData}
+                        data={searchResults}
+                    />
+                </div>
+
+                <div className='weather--data'>
+                    { displayData && <WeatherCard data={cityData} /> }
+                </div>
+
+
+                {/* {
+                    city === '' ?
+                    null :
+                    <SearchResults setCity={setCity} getData={getData} data={searchResults} />
+                } */}
+                {/* <button className='themer posLeft' onClick={changeTheme}>
+                {
+                    theme === '' ?
+                    "✏️" :
+                    "✏️"
+                }
+                </button> */}
+                <button className='themer posRight btn--invert' onClick={() => clearData()}>Clear</button>
+            </div>
         </div>
     );
 }
--- src/Input/Input.component.js
+++ src/Input/Input.component.js
@@ -3,18 +3,39 @@ import React from 'react';
 import './Input.styles.css';
 
 const Input = props => {
-    const { city, setCity, getData } = props;
+    const { city, setCity, getData, data } = props;
+
+    const cityParam = (e) => {
+        setCity(e.city);
+        getData(city);
+    }
 
     return (
-        <div>
+        <div className='input--container'>
             <input 
                 type="text"
-                placeholder="Enter a city..."
+                placeholder="Start typing a city..."
                 value={city}
                 onChange={(e) => setCity(e.target.value)}
                 onKeyPress={(e) => e.key === 'Enter' ? getData(e.target.value) : null}
                 className="input--control"
+                list='cityList'
+                autoFocus
             />
+            <datalist className='city--datalist' id='cityList'>
+                {
+                    data.map((elem, index) => 
+                        <option 
+                            className='search--parameter' 
+                            key={index} 
+                            datastore={elem.city} 
+                            onClick={() => 
+                                cityParam(elem)} 
+                        >
+                                {elem.city}, {elem.country}
+                        </option>)
+                }
+            </datalist>
         </div>
     )
 }
